f7ebffd23ee07477e1b84b61f8a2570e78c21e37
joekennerly <joseph.kennerly@gmail.com>
2019-10-26 20:09:45 -0500

    added product view

--- kterapi/views/__init__.py
+++ kterapi/views/__init__.py
@@ -1,3 +1,4 @@
 from .register import register_user, login_user
 from .vendor import Vendors
 from .productcategory import ProductCategories
+from .product import Products
---
+++ kterapi/views/product.py
@@ -0,0 +1,82 @@
+"""View module for handling requests about product category"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from kterapi.models import Product, Vendor, ProductCategory
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for product"""
+    class Meta:
+        model = Product
+        url = serializers.HyperlinkedIdentityField(
+            view_name='product',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'name', 'vendor', 'productcategory', 'price', 'description')
+
+class Products(ViewSet):
+    """Products for KTER"""
+
+    def create(self, request):
+        product = Product()
+
+        vendor = Vendor.objects.get(user=request.auth.user)
+        productcategory = ProductCategory.objects.get(
+            pk=request.data['productcategory_id'])
+
+        product.vendor = vendor
+        product.productcategory = productcategory
+        product.name = request.data["name"]
+        product.price = request.data["price"]
+        product.description = request.data["description"]
+        product.save()
+
+        serializer = ProductSerializer(product, context={'request': request})
+
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        try:
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+
+        vendor = Vendor.objects.get(user=request.auth.user)
+        productcategory = ProductCategory.objects.get(
+            pk=request.data['productcategory_id'])
+
+        product.vendor = vendor
+        product.productcategory = productcategory
+        product.name = request.data["name"]
+        product.price = request.data["price"]
+        product.description = request.data["description"]
+        product.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        try:
+            product = Product.objects.get(pk=pk)
+            product.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Product.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def list(self, request):
+        product = Product.objects.all()
+
+        serializer = ProductSerializer(
+            product, many=True, context={'request': request})
+        return Response(serializer.data)
