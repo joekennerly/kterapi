c197c6777fc20ea7594e893d6b94df226f84301b
joekennerly <joseph.kennerly@gmail.com>
2019-11-01 12:58:21 -0500

    can delete order

--- kterapi/models/OrderProduct.py
+++ kterapi/models/OrderProduct.py
@@ -5,7 +5,7 @@ from .Product import Product
 class OrderProduct(models.Model):
 
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("orderproduct")
